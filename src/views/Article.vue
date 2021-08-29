5c9bca411a28fdfcf30b7e988044c23dd3eb4844
korvallol <korolevavv@lebedev.ru>
2021-08-29 22:58:36 +0300

    weekend...

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="article">
     <div class="articleTitle">{{ title }}</div>
-    <div class="fullText" id="fullText">
-      <!-- <p class="paragraph" v-for="(par, i) in fulltext" :key="i">
-        {{ par }}
-      </p> -->
-    </div>
+    <div class="fullText" id="fullText"></div>
     <div class="attachment" id="attachments"></div>
-    <!-- <a :href=link>test</a> -->
   </div>
 </template>
 
 <script>
 import csv from "@/assets/questions.csv";
+import gloss from "@/assets/glossary.csv";
+
 import { useRoute } from "vue-router";
+import { Popover } from "bootstrap";
+
 
 export default {
   data() {
@@ -23,10 +22,21 @@ export default {
       attachment: String,
       elementSaved: Object,
       route: Object,
-      // link: require("../assets/pdfs/test.pdf")
+      glossary: [],
     };
   },
   props: ["id"],
+  created() {
+    for (let i = 0; i < gloss.length; i++) {
+      let element = gloss[i];
+      this.glossary[i] = {
+        id: element.id,
+        term: element.term,
+        short: element.short,
+        long: element.long,
+      };
+    }
+  },
   mounted() {
     const route = useRoute();
     let element = csv[route.query.id];
@@ -39,6 +49,16 @@ export default {
       .getElementById("fullText")
       .insertAdjacentHTML("afterbegin", copyAnswer);
 
+    // enable popover
+    var popoverTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="popover"]')
+    );
+    popoverTriggerList.map(function (popoverTriggerEl) {
+      return new Popover(popoverTriggerEl, {container: 'body', html: true});
+    });
+    console.log(document.querySelectorAll('[data-bs-toggle="popover"]')[0])
+
+    //
     this.fulltext = element.answer.split("\n");
     this.attachment = "";
 
@@ -79,8 +99,8 @@ export default {
       const exSiteStringReg = /\(http(\S+)/gi;
       let exSiteStrings = [];
       if (exSiteStringReg.test(text)) {
-        let stringBefore = text.split("(http")[0];
-        console.log(stringBefore.split(' ').slice(0, -1)[0])
+        // let stringBefore = text.split("(http")[0];
+        // console.log(stringBefore.split(' ').slice(0, -1)[0])
         exSiteStrings = text.match(exSiteStringReg);
       }
       exSiteStrings.forEach((el) => {
@@ -108,6 +128,50 @@ export default {
         let linkHTML = link.outerHTML;
         copyAnswer = copyAnswer.replace(el, linkHTML);
       });
+
+      const glossaryReg = /\(glossary_\d+\)/gi;
+      let glossaryStrings = [];
+      if (glossaryReg.test(text)) {
+        glossaryStrings = text.match(glossaryReg);
+      }
+      glossaryStrings.forEach((el) => {
+        let id = el.split("_")[1];
+        id = id.slice(0, -1);
+        let termObject = this.glossary.find((ob) => {
+          if (ob.id == id) {
+            return ob.term;
+          }
+        });
+        let term = termObject.term;
+        let link = document.createElement("a");
+        let linkTerm = document.createElement("a");
+        let linkTermHTML = "";
+        if (termObject.long.length > 0) {
+          let href = "/term?id=" + termObject.id;
+          linkTerm.href = href;
+          link.innerHTML = "Подробнее";
+          linkTermHTML = linkTerm.outerHTML;
+        }
+        link.setAttribute("tabindex", 0);
+        link.setAttribute("id", "gloss-" + id);
+        link.setAttribute("data-bs-toggle", "popover");
+        link.setAttribute("data-bs-trigger", "click");
+        link.title = term;
+        let pop = document.createElement('div')
+        pop.setAttribute('class', 'popover-body')
+        let popText = document.createElement('p')
+        popText.innerHTML = termObject.short
+        pop.append(popText)
+        pop.append(linkTerm)
+        link.setAttribute("data-bs-content", pop.outerHTML);
+        console.log(linkTermHTML)
+        // link.setAttribute("data-bs-template", termObject.short + linkTermHTML);
+        // console.log(link)
+        link.innerHTML = "(См. " + term + ")";
+        let linkHTML = link.outerHTML;
+        copyAnswer = copyAnswer.replace(el, linkHTML);
+      });
+
       return copyAnswer;
     },
   },
