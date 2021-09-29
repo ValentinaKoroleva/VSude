036bb74918ab614c126fac3e110b76cfa5f12ea3
korvallol <korolevavv@lebedev.ru>
2021-09-29 15:07:54 +0300

    minor design

--- src/router/index.js
+++ src/router/index.js
@@ -53,8 +53,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/WhatIsCourtMon.vue'),
-    // props: true,
-    props: (route) => ({ query: route.query.q })
+  },
+  {
+    path: '/contacts',
+    name: 'Contacts',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue'),
   },
   {
     path: '/glossary',
---
+++ src/views/Contact.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="about d-flex justify-content-center">
+    <!-- <div class="logo-header">
+      <img src="../assets/logo.png" alt="logo" />
+    </div> -->
+    <div class="articleBody">
+      <p class="articleTitle">Гражданский контроль</p>
+      <p class="contentHeader">
+        <i class="bi bi-telephone-fill"></i> Контактный телефон:
+      </p>
+      <p class="content">
+        <a href="tel:+78123092669">8 (812) 309-26-69</a>
+      </p>
+      <p class="contentHeader">
+        <i class="bi bi-envelope-open"></i>Почта для связи:
+      </p>
+      <p class="content">
+        <a href="mailto:citwatchspb@gmail.com">citwatchspb@gmail.com</a>
+      </p>
+      <p class="contentHeader">
+        <i class="bi bi-envelope-open-fill"></i>Юридическая служба:
+      </p>
+      <p class="content">
+        <a href="mailto:jurist@citwatch.org">jurist@citwatch.org</a>
+      </p>
+      <p class="contentHeader"><i class="bi bi-people"></i>Сайт:</p>
+      <p class="content">
+        <a href="https://citwatch.org/">https://citwatch.org/</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+p {
+  text-align: left;
+}
+.articleTitle {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.25rem;
+  line-height: 2em;
+  text-align: left;
+  /* margin-bottom: 5vh; */
+  text-decoration-style: solid !important;
+}
+.content {
+    /* align-self:center */
+    /* margin-left: 20% */
+}
+.contentHeader {
+}
+.articleBody {
+    padding-right: 10%;
+    padding-top: 3%;
+    padding-bottom: 3%;
+    padding-left: 10%;
+    border: 1px solid #74C7C5;
+    border-radius: 18px;
+    background-color:white;
+}
+.about {
+  padding: 5%;
+  margin: 5%;
+  margin-top: 0%;
+  padding-top: 0%;
+  background: white;
+  z-index: 999;
+  display: inline-block;
+  height: auto;
+  border-radius: 18px;
+  width: 90%;
+  background-color:#e9f7f9
+}
+.bi {
+  margin:5%!important;
+}
+img {
+  width: 10%;
+  height: 10%;
+}
+.logo-header {
+  display: flex;
+  float: left;
+}
+</style>
