79b2b5c56ef075bfc8e8ab20472acbc701f95c2d
korvallol <korolevavv@lebedev.ru>
2021-10-01 13:34:58 +0300

    minor changes

--- src/components/PhoneImg.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <img src="@/assets/phone.png" alt=""/>
-</template>
-
-<style scoped>
-    /* img {
-         background-image: url(../assets/volcano.jpg) !important; 
-    } */
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -19,14 +19,6 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/online',
-    name: 'Online',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/CourtOnline.vue')
-  },
-  {
     path: '/article/:category',
     name: 'Article',
     // route level code-splitting
