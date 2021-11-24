7fb59b1bea4f63c3ab6da38bc446e964a92ce128
ValentinaKoroleva <korolevavv@lebedev.ru>
2021-11-24 16:27:20 +0300

    pwa

--- vue.config.js
+++ vue.config.js
@@ -34,13 +34,8 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
 
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
+
+
   }
 
   // publicPath: '/'
