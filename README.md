6c6e0b9135e53f1cc15a047906b9e7c00f291c9b
ValentinaKoroleva <korolevavv@lebedev.ru>
2021-11-24 15:39:12 +0300

    clear cache

--- README.md
+++ README.md
@@ -26,3 +26,4 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 
 ### Firebase Deploy
 firebase init
+
--- vue.config.js
+++ vue.config.js
@@ -27,20 +27,21 @@ module.exports = {
       .loader("file-loader")
 
   },
-  // pwa: {
-  //   name: 'My App',
-  //   themeColor: '#4DBA87',
-  //   msTileColor: '#000000',
-  //   appleMobileWebAppCapable: 'yes',
-  //   appleMobileWebAppStatusBarStyle: 'black',
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
 
-  //   // configure the workbox plugin
-  //   workboxPluginMode: 'InjectManifest',
-  //   workboxOptions: {
-  //     // swSrc is required in InjectManifest mode.
-  //     swSrc: 'dev/sw.js',
-  //     // ...other Workbox options...
-  //   }
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 
   // publicPath: '/'
 };
